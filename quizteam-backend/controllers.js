92e7e179940feb6f4be9a57c682eccf50a4f416e
astrocu6er <astrocu6er@gmail.com>
2018-05-31 17:32:38 -0400

    Installed some stuff

--- quizteam-backend/controllers.js
+++ quizteam-backend/controllers.js
@@ -9,16 +9,13 @@ const Rooms = mongoose.model('Rooms');
   Creates a new room w/ random room code 100000-999999, random adminSecret
 */
 exports.createRoom = (req, res) => {
-
-  if (req.body.quizlet_set_id == null) {
-    res.json({resp_code: 1, resp_msg: "null parameters"});
-    return;
-  }
   
   // generate random number
   var roomCode = Math.floor(Math.random() * (999999 - 100000 + 1)) + 100000;
-
   var adminSecret;
+  // generate random number
+ 
+  
   // while check the other roomcodes to make sure its not taken
   
   var newRoom = new Room({
@@ -39,14 +36,6 @@ exports.createRoom = (req, res) => {
 // deletes the room
 exports.destroyRoom = (req, res) => {
   
-  if (req.body.adminSecret == null || req.body.roomCode == null) {
-    res.json({resp_code: 1, resp_msg: "null parameters"});
-    return;
-  }
-  
-  Rooms.findOneAndDelete({adminSecret: req.body.adminSecret, roomCode: req.body.roomCode}, (removedRoom) => {
-    res.json({resp_code: 100});
-  });
 }
 
 
