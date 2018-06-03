82b87b0fe8656cb023b88a2ee27841d44f570456
hypercubestart <andrewlliu@gmail.com>
2018-06-03 02:44:48 -0700

    test

---
+++ quizteam-app/src/api.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import querystring from 'query-string';
+
+const baseUrl = 'http://138.197.207.244:3001/'
+const quizletURLRegex = RegExp('(https:\/\/quizlet.com\/)[0-9]+');
+
+var createRoom = function(quizletUrl, callback) {
+    const url = baseUrl + 'api/room/create';
+
+    if (!quizletURLRegex.test(quizletUrl)) {
+        callback({
+            status: 1
+        });
+        return false;
+    }
+    
+    var second = quizletUrl.split('quizlet.com/')[1];
+
+    var id = second.substring(0, second.indexOf('/'));
+    
+    var data = querystring.stringify({
+        quizletSetID: id
+    });
+    axios.post(url, data).then((resp) => {
+        console.log(resp);
+    });
+    
+}
+
+export { createRoom };
