3c959160c011b4b3d72e1cb64b2c6018bd8bb3a8
hypercubestart <andrewlliu@gmail.com>
2018-06-03 03:42:22 -0700

    hi

--- quizteam-app/src/sockets.js
+++ quizteam-app/src/sockets.js
@@ -1,6 +1,6 @@
 import openSocket from 'socket.io-client';
 
-const socket = openSocket('http://138.197.207.244/');
+const socket = openSocket('http://138.197.207.244:3000/');
 
 export default socket;
 
