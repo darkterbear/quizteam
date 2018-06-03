d8668161008add73aca6610ad9e6244192edd2cd
Terrance Li <terranceli.terbear@gmail.com>
2018-06-02 21:14:43 -0700

    meme

--- quizteam-app/src/splash/Splash.js
+++ quizteam-app/src/splash/Splash.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import {
+    AwesomeButton,
+    AwesomeButtonProgress,
+    AwesomeButtonShare,
+} from 'react-awesome-button';
+import 'react-awesome-button/dist/styles.css';
 
 export default class Splash extends Component {
     render() {
         return(
             <div className="container vcenter">
                 <h1><blue>quiz</blue><green>team</green></h1>
+                <AwesomeButton type="primary"><buttontext>join room</buttontext></AwesomeButton>
             </div>
         );
     }
