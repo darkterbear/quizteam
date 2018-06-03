bef2edea1cd7b4e7f7b3be8423709d62f123770a
FutureBeyondAtmosphere <FutureBeyondAtmosphere@gmail.com>
2018-06-02 23:21:13 -0700

    white fix

--- quizteam-app/src/waiting-room/WaitingRoom.js
+++ quizteam-app/src/waiting-room/WaitingRoom.js
@@ -12,7 +12,7 @@ export default class WaitingRoom extends Component {
             <div className="container vcenter">
                 <h2><blue>Waiting...  </blue></h2>
                 <h4><blue>be patient :)</blue></h4>
-                <h5><white>cards will show up on the board. when any of your cards match any of the displayed cards, click on it!<br/><br/> there may be terms you dont know... so be sure to communicate and work together... as a </white><blue>quizteam</blue></h5>
+                <h5>cards will show up on the board. when any of your cards match any of the displayed cards, click on it!<br/><br/> there may be terms you dont know... so be sure to communicate and work together... as a <blue>quizteam</blue></h5>
                 </div>
         );
     }
