e677a49f17da531fb5cdb1ddc895f1dcfed31d9c
Terrance Li <terranceli.terbear@gmail.com>
2018-06-02 22:39:28 -0700

    meme

--- quizteam-app/src/App.js
+++ quizteam-app/src/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 import Splash from './splash/Splash';
 import CreateRoom from './create-room/CreateRoom';
 import JoinRoom from './join-room/JoinRoom';
-import WaitingRoom from '.waiting-room/WaitingRoom';
+import WaitingRoom from './waiting-room/WaitingRoom';
+import MasterStandby from './master-standby/MasterStandby';
 
 export default class App extends React.Component {
 
     render() {
         return (
             //<Splash/>
+            //<CreateRoom/>
+            //<JoinRoom/>
             <WaitingRoom/>
+            //<MasterStandby roomCode='meme'/>
         )
     }
 }
--- quizteam-app/src/master-standby/MasterStandby.js
+++ quizteam-app/src/master-standby/MasterStandby.js
@@ -9,8 +9,14 @@ import 'react-awesome-button/dist/styles.css';
 export default class MasterStandby extends Component {
     render() {
         return (
-            <div className="container vcenter">
-                <AwesomeButton type="secondary" style={{ marginTop: '32px' }}><buttontext>start game</buttontext></AwesomeButton>
+            <div id="root">
+                <div className="container">
+                    <h2><green>{this.props.roomCode}</green></h2>
+                </div>
+
+                <div className="container vcenter">
+                    <AwesomeButton type="secondary" style={{ marginTop: '32px' }}><buttontext>start game</buttontext></AwesomeButton>
+                </div>
             </div>
         );
     }
