0d342f1e515b7260e3805101367b994b15ecc653
FutureBeyondAtmosphere <FutureBeyondAtmosphere@gmail.com>
2018-06-02 22:35:28 -0700

    forgot a slash

--- quizteam-app/src/App.js
+++ quizteam-app/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Splash from './splash/Splash';
 import CreateRoom from './create-room/CreateRoom';
 import JoinRoom from './join-room/JoinRoom';
-import WaitingRoom from '.waiting-room/WaitingRoom';
+import WaitingRoom from './waiting-room/WaitingRoom';
 
 export default class App extends React.Component {
 
