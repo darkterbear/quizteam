5dfed929e1481a5a15eaaeb6d2e949958b4fee67
Terrance Li <terranceli.terbear@gmail.com>
2018-06-03 00:54:54 -0700

    fix

--- quizteam-app/src/master-screen/Card.js
+++ quizteam-app/src/master-screen/Card.js
@@ -4,7 +4,7 @@ export default class Card extends Component {
     render() {
         return (
             <div style={{ width: '40%', height: '100%', background: '#ffffff', float: 'left', marginLeft: '5%', marginRight: '5%'}}>
-                <h3 style={{ color: '#000000', marginTop: '20%'}}>{this.props.text}</h3>
+                <h3 style={{ color: '#000000', top: '35%', position: 'relative'}}>{this.props.text}</h3>
             </div>
         );
     }
