1e93c81bdd4a0dc9746b23373497970ab142a276
Terrance Li <terranceli.terbear@gmail.com>
2018-06-02 20:45:59 -0700

    added init

---
+++ quizteam-app/src/splash/Splash.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Splash extends Component {
+    render() {
+        return(
+            <div className="container vcenter">
+                <h1><blue>quiz</blue><green>team</green></h1>
+            </div>
+        );
+    }
+}
