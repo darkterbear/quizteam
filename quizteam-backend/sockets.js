876de31222fe4eb153d6b28881c353a768c86572
Terrance Li <terranceli.terbear@gmail.com>
2018-06-02 17:30:45 -0700

    added setroom socket

--- quizteam-backend/sockets.js
+++ quizteam-backend/sockets.js
@@ -1,9 +1,11 @@
 // array that holds rooms and socket clients
 /*
 {
+    roomCode: Number
     admin: {Socket},
     players: [Socket],
-    currentlyShownCards: [String] the id's
+    availableCards: [Card]
+    currentlyShownCards: [Card]
 }
 */
 var rooms = [];
@@ -18,22 +20,45 @@ module.exports = (io) => {
         // called by the room admin to assert as admin
         // creates the room object and stores in the rooms array
         client.on('roomAdmin', (room, adminSecret) => {
-            Rooms.findOne({roomCode: room, adminSecret: adminSecret}, (room) => {
+            Rooms.findOne({roomCode: room, adminSecret: adminSecret}, (err, room) => {
+                if (err || room == null) {
+                    client.emit('roomAdminResp', 'unauthorized/room not found');
+                    return;
+                }
+                var newRoom = {
+                    roomCode: room.roomCode,
+                    admin: client,
+                    players: [],
+                    availableCards: room.cards,
+                    currentlyShownCards: []
+                };
 
+                rooms.push(room);
+                client.emit('roomAdminResponse', 'success');
             });
         });
 
         // called by player to join the room
         client.on('setRoom', (room) => {
             client.join(room, () => {
+                var room = rooms.find((testRoom, i, rooms) => {
+                    if (testRoom.roomCode == room.roomCode) return true;
+                    return false;
+                });
 
+                if (room == null) {
+                    client.emit('setRoomResponse', 'room doesnt exist');
+                } else {
+                    room.players.push(client);
+                    client.emit('setRoomResponse', 'success');
+                }
             });
         });
 
         // called by player to submit an action
         client.on('submitAction', (room, action) => {
 
-        })
+        });
 
         // called by the player leaving the room
         client.on('disconnect', () => {
