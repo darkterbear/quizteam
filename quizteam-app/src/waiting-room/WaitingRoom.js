dc6aef2dbbe5befdfcba9970da4e95bc74459043
FutureBeyondAtmosphere <FutureBeyondAtmosphere@gmail.com>
2018-06-02 23:47:31 -0700

    added changing waiting

--- quizteam-app/src/waiting-room/WaitingRoom.js
+++ quizteam-app/src/waiting-room/WaitingRoom.js
@@ -7,10 +7,30 @@ import {
 import 'react-awesome-button/dist/styles.css';
 
 export default class WaitingRoom extends Component {
+
+    constructor(props) {
+        this.state.waitingText = 'Waiting...';
+
+        this.changeText = this.changeText.bind(this);
+
+        this.changeText();
+    }
+
+    changeText() {
+        var newText = this.state.waitingText;
+        if (newText.contains('...')) newText = 'Waiting.';
+        else if (newText.contains('..')) newText = 'Waiting...';
+        else newText = 'Waiting..';
+        this.setState({waitingText: newText});
+        setInterval(changeText(), 1000);
+    }
+
+
+
     render() {
         return (
             <div className="container vcenter">
-                <h2><blue>Waiting...  </blue></h2>
+                <h2><blue>{this.state.waitingText}</blue></h2>
                 <h4><blue>be patient :)</blue><br/><br/></h4>
                 <div className="instructions">
                     <h5>cards will show up on the board. when any of your cards match any of the displayed cards, click on it!<br/><br/> there may be terms you dont know... so be sure to communicate and work together... as a <blue>quizteam</blue></h5>
