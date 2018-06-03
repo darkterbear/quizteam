96831242c85c4e06082b537468764fd22979dcd4
hypercubestart <andrewlliu@gmail.com>
2018-06-03 00:19:20 -0700

    quickfix

--- quizteam-app/src/player-screen/PlayerScreen.js
+++ quizteam-app/src/player-screen/PlayerScreen.js
@@ -3,7 +3,9 @@ import React, { Component } from 'react';
 export default class PlayerScreen extends Component {
     render() {
         return(
-            
+            <div>
+                hi
+            </div>
         )
     }
 }
