98727d972e2a52e35b159ee2543f614f616d5408
astrocu6er <astrocu6er@gmail.com>
2018-06-04 18:16:40 -0400

    omg

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,9 @@
+if [[ $UID != 0 ]]; then
+    echo "Please run this script with sudo:"
+    echo "sudo $0 $*"
+    exit 1
+fi
 cd ..
-if [[ ! $(sudo echo 0) ]]; then exit; fi
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
 sudo apt-get update
