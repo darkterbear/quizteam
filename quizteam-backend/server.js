cffdbac21183b73f6f917d136857f22d40f811c1
Terrance Li <terranceli.terbear@gmail.com>
2018-06-12 10:20:54 -0700

    tesT

--- quizteam-backend/server.js
+++ quizteam-backend/server.js
@@ -25,7 +25,8 @@ app.use(bodyParser.json());
 app.use(cors());
 app.use('/static', express.static(path.join(__dirname, 'public')))
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://quizteam.dsys32.com");
+    res.header("Access-Control-Allow-Origin", "*");
+    //res.header("Access-Control-Allow-Origin", "http://quizteam.dsys32.com");
     res.header("Access-Control-Allow-Credentials", "true");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
